384ee4381786742ec2a4ddb1cc39ec2adc6ca83c
jocile <jocile@gmail.com>
2024-12-05 16:58:24 -0300

    Atualizando o settings

--- djangotutorial/mysite/settings.py
+++ djangotutorial/mysite/settings.py
@@ -25,12 +25,13 @@ SECRET_KEY = 'django-insecure-$r+e-@f391&v_@mm224*mtkp=zc$%&t3wub9%unau#h@%z1s@*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    "polls.apps.PollsConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
